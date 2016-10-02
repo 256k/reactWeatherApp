f11a47ffb03291f58abb6c2ca0015abb486f253e
256k <fayek.helmi@gmail.com>
2016-10-01 22:02:18 -0400

    modified express server.js file to filter http requests and convert any https request to http for use with openweathermap.

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require("express"); // requiring this means we save the entire exp
 
 // create our app
 var app = express(); // we run the entire express library as a function.
+const PORT = process.env.PORT || 3000;
+app.use(function(req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    } else{
+        res.redirect("http://" + req.hostname + req.url);
+    }
+});
 app.use(express.static('public')); // this defines the directory it will use to show the files
-app.listen(3000, function(){ // this is to start the server. it takes 2 arguments: a port number and a function.
-    console.log("express server is up on port 3000");
+app.listen(PORT, function(){ // this is to start the server. it takes 2 arguments: a port number a function.
+    console.log("express server is up on port "+ PORT);
 });
