7e23d7074d2b29022dfe35005e8b61967805f82e
256k <fayek.helmi@gmail.com>
2016-10-02 14:17:31 -0400

    added foundation styles for the nav component

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -3,11 +3,18 @@ var {Link, IndexLink} = require('react-router');
 
 var Nav = (props) => {
         return (
-            <div>
-                <h2>Nav Component</h2>
-                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Get weather</IndexLink>
-                <Link to="/about" activeClassName="active" activeStyle={{fontWeight: "bold"}}>About</Link>
-                <Link to="/ex" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Examples</Link>
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                    <li className="menu-text">Weather App</li>
+                        <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Get weather</IndexLink></li>
+                        <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: "bold"}}>About</Link></li>
+                        <li><Link to="/ex" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Examples</Link></li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+
+                </div>
             </div>
         );
     };
