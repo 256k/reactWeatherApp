0428c0b3ad02d2ae2b89311c5d3367e7be01b3aa
256k <fayek.helmi@gmail.com>
2016-10-01 22:37:15 -0400

    adjusted about and examples pages. and added some margin between the nav links

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
         return(
             <div>
-            <h3>About Component</h3>
+            <h3>About</h3>
             <p>This is a React app demo project following the Udemy React course.<br/>
             this app fetches the weather information of a chosen city. you can type your city in the search box and the app will tell you the weather in this city.<br/>
             thank you for using this demo.</p>
--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -2,7 +2,10 @@ var React = require("react");
 
 var Examples = (props) => {
         return(
-                <h1>Examples page!</h1>
+            <div>
+                <h1>Examples</h1>
+                <p>There are currently no examples</p>
+            </div>
         )
     };
 
